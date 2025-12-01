f0a2fe57defacfa515456d895022a469aab9d284
Rafael Santos de Mendonça <rafasmendonca007@gmail.com>
2025-12-01 18:13:04 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3:
+        return None
+    if parts[0] != "cp":
+        return None
+    src = parts[1]
+    dst = parts[2]
+    if src == dst:
+        return None
+    if not os.path.exists(src) and not os.path.isfile(src):
+        return None
+    with open(src, "rb") as fsrc, open(dst, "wb") as fdst:
+        chunk = fsrc.read(4096)
+        while chunk:
+            fdst.write(chunk)
+            chunk = fsrc.read(4096)
